de55fb09b91e1fcf387d73e85efb0d33f87eb549
mazuisan <44691452+mazuisan@users.noreply.github.com>
2019-09-28 00:23:45 -0400

    hopefully

---
+++ U.N.OWEN/Assets/README.txt
@@ -0,0 +1,4 @@
+﻿0927from amy
+notes for imported package of my last game jam
+cinemachine and 2d light are from unity library
+other unused assets will delete as soon as possible
---
+++ U.N.OWEN/Assets/Scripts/AreaExit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+    [SerializeField] string areaToLoad;
+    public string areaTransitionName;//用来标注是从哪个门出来的 这样让角色初识时站在正确的位置
+                                     //1-1-1 means the first door in scene1-1
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerController.instance.areaTransitionName = areaTransitionName;
+            Debug.Log("we are trying to load " + areaToLoad);
+            SceneManager.LoadScene(areaToLoad);
+            
+        }
+    }
+}
