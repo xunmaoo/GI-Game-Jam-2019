46dc2d54b3921ffce70d0fe7797130798e8db7bb
tieuchanlong <tieuchanlong@gmail.com>
2019-09-27 21:25:40 -0400

    Enemy movement added

---
+++ U.N.OWEN/Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    public GameObject player;
+    public float step = 0.1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+    }
+}
